e9933c085a2fadc2aee62831c0626604bd16eef7
beebooplena <lena_kath@hotmail.com>
2022-04-14 14:29:35 +0000

    created database tasks and categories

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from taskmanager import app
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,3 +1,4 @@
+
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -5,10 +6,9 @@ if os.path.exists("env.py"):
     import env  # noqa
 
 
-
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] =os.environ.get("DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
